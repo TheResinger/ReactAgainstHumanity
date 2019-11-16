5031baddf16437d644c7e70cd7f61497143603b3
Benjamin Morrow <morrowbenjamin42@gmail.com>
2019-11-16 02:48:18 -0500

    added production build

---
+++ client/build/precache-manifest.68d1b6016da85595e8aa8e15fdd34f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39107c892386606aef6eef77a85d1bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66292579731195d500a6",
+    "url": "/static/css/main.b3243b72.chunk.css"
+  },
+  {
+    "revision": "1d1d7b61f326e05f233c",
+    "url": "/static/js/2.a17e427e.chunk.js"
+  },
+  {
+    "revision": "66292579731195d500a6",
+    "url": "/static/js/main.017ad91d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
